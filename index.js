1ab7d6d391dcbd6607dd58e5a9110bd1e9466437
manel turki <menelturki@gmail.com>
2020-01-21 13:07:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const pets=["Milo", "Otis", "Garfield"];
-const appends=["Milo", "Otis", "Garfield"];
+const append=["Milo", "Otis", "Garfield"];
+const prepend=s=["Milo", "Otis", "Garfield"];
+const 
 pet.push('Odie');
 pet.unshift('prepends');
 pet.pop();
